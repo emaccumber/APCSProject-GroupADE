a27a21bb2e6b0534033b7c804af46a4334d0f31b
ethanmaccumber <ethanmacc@gmail.com>
2016-08-01 11:33:00 -0500

    I've added and changed some things

--- src/Box.java
+++ src/Box.java
@@ -4,24 +4,29 @@ import java.awt.Color;
 
 public class Box extends GraphicsProgram
 {
-	
+	//private GImage background;
+
 	
 	public void init()
 	{
 		setSize(536, 800);
+		//background = new GImage("BoardTemplate.jpg", 0, 0);
+		//background.setSize(getWidth(), getHeight());
 	}
 
 	public void run() 
 	
 	{
+		
+		//add(background);
 		GPolygon poly = new GPolygon();
 	      poly.setFilled(true);
 	      poly.setColor(Color.RED);
 	      
-	      poly.addArc(100, 100, 100, 100);
+	      poly.addArc(200, 200, 3.14, 100);
 	      poly.addEdge(100, 100);
 	      
-	      poly.setLocation(100, 100);
+	      poly.setLocation(200, 200);
 	      add(poly);
 		
 	}
